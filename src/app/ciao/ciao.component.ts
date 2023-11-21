8755a376e6283d5d4afbac55bbf266032b3e7e8c
ele-3-car <eleonora.carbini2003@gmail.com>
2023-11-21 15:08:53 +0100

    aggiunta componente

---
+++ src/app/ciao/ciao.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-ciao',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ciao.component.html',
+  styleUrl: './ciao.component.scss'
+})
+export class CiaoComponent {
+
+}
